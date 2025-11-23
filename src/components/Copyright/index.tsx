62bdc768aac19950bb617c77720a5eb32de07793
Omar Othon Soto Sainz <omarsotosainz@gmail.com>
2025-11-23 13:41:18 -0700

    fix: se ajusto el Copyright

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -7,21 +7,23 @@ const Copyright = () => {
     <div className="container-fluid copyright py-4">
       <div className="container">
         <div className="row g-4 align-items-center">
-          <div className="col-md-6 text-center text-md-start mb-md-0">
+          <div className="col-md-4 text-center text-md-start mb-md-0">
             <span className="text-white">
               <a href="#">
                 <i className="fas fa-copyright text-light me-2"></i>Interser -
                 Centro Psicoterapeutico Humanista
-              </a>{" "}
-              - Todos los derechos reservados.
+              </a>
             </span>
           </div>
-          <div className="col-md-6 text-center text-md-end text-white">
-            Designed By
+          <div className="col-md-3 text-center text-md-start mb-md-0">
+            <span className="text-white">Todos los derechos reservados.</span>
+          </div>
+          <div className="col-md-5 text-center text-md-end text-white">
+            Designed By{" "}
             <a className="border-bottom" href="https://htmlcodex.com">
               HTML Codex
-            </a>
-            Distributed By
+            </a>{" "}
+            Distributed By{" "}
             <a className="border-bottom" href="https://themewagon.com">
               ThemeWagon
             </a>
